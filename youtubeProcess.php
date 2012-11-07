19aad76887d435a52e42ef8d7e8fcb39b771049f
lilbro1062000 <lilbro1062000@gmail.com>
2012-11-07 17:53:08 -0500

    update

--- youtubeProcess.php
+++ youtubeProcess.php
@@ -1,32 +1,51 @@
 <?php
-    
-    // this will parse the urls and the get 
-    
-    // first generate the url 
+
+// this will parse the urls and the get
+
+// first generate the url
 require_once "simple_html_dom.php";
 require_once 'connection.php';
 require_once "functions.php";
-    
-  $row = ex_query1Row("select * from ProcessVideos where processed ='No'");
-  $html = file_get_html('http://www.youtube.com'.$row["url"]);
-foreach ($html->find('div[id=watch-container]') as $element) {
-	
 
-		$s = explode('<', $element);
-		
-		for ($q = 0; $q < count($s); $q++) {
-			if (strlen(strstr($s[$q], "met")) > 0) {
-					
-				$st =substr($s[$q], 0,100000);
-				
-				var_dump($st);
+$row = ex_query1Row("select * from ProcessVideos where processed ='No'");
+$html = file_get_html('http://www.youtube.com' . $row["url"]);
+//foreach ($html->find('div[id=watch-container]') as $element) {
+	foreach ($html->find('head') as $element) {
+
+	$s = explode('<', $element);
+
+	for ($q = 0; $q < count($s); $q++) {
+		$mainstring = $s[$q];
+		//var_dump($mainstring);
+		if (strlen(strstr($mainstring, "meta property=\"og:")) > 0) {
+				var_dump($mainstring);
+			$st = substr($mainstring, 0, 100000);
+			// if (strlen(strstr($mainstring, "meta property=\"og:")) > 0) {
+			// //	$name = $mainstring;
+			// //	$name = substr($name, 34, strlen($name) - 41);
+			// //	echo "$name " . strlen($name);
+			// echo "$mainstring \n";
+// 
+			// } 
+			
+			if (strlen(strstr($mainstring, "meta property=\"og:title\"")) > 0) {
+				$name = $mainstring;
+				$name = substr($name, 34, strlen($name) - 41);
+				echo "$name " . strlen($name);
+
+			} 
+			if (strlen(strstr($mainstring, "meta property=\"og:description\"")) > 0) {
+				$description = $mainstring;
+				$description =substr($description, 30, strlen($description) - 37);
+				echo "\n";
+				echo "$description";
+			}
 		}
 	}
-	
 }
 
 // after ward we need to get some information into the db and have it translate
-// first is the embeded link  
+// first is the embeded link
 
 //id="watch-container"
 //echo $html;
